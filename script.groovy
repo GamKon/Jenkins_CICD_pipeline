ff64fcab5a732eda3b2163dc8e7d786c1b51b052
GamKon <gamkon61@gmail.com>
2021-10-13 18:10:09 -0400

    test deployment stage

--- script.groovy
+++ script.groovy
@@ -41,7 +41,8 @@ def deployApp() {
     echo 'deploying the application to EC2...'
 //    echo "EC2 piblic IP: $EC2_PUBLIC_IP"
     def docker_command = "docker run -p 8080:8080 -d gamkon-repo:jma-1.0"
-// ${APP_IMAGE_NAME}"   ${EC2_PUBLIC_IP}
+// ${APP_IMAGE_NAME}"   
+// ${EC2_PUBLIC_IP}
     sshagent({'key_for_ec2'}) {
         sh "ssh -o StrictHostKeyChecking=no ec2-user@35.182.226.1 ${docker_command}"
     }
